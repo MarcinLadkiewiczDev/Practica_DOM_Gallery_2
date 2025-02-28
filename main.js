aa80804eebdd2d102dd18971a29d29a17d9be144
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-02-28 13:44:36 +0100

    Creating files

---
+++ main.js
@@ -0,0 +1,171 @@
+const personajesMarvel = [
+  {
+    nombreSuperheroe: "Iron Man",
+    nombreReal: "Tony Stark",
+    primeraAparicion: "Iron Man (2008)",
+    poder:
+      "Genio, multimillonario, filántropo con armadura tecnológica avanzada",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Capitán América",
+    nombreReal: "Steve Rogers",
+    primeraAparicion: "Captain America: The First Avenger (2011)",
+    poder:
+      "Fuerza, velocidad y resistencia sobrehumanas gracias al suero del supersoldado",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Thor",
+    nombreReal: "Thor Odinson",
+    primeraAparicion: "Thor (2011)",
+    poder: "Dios del trueno, fuerza sobrehumana, control del clima y Mjolnir",
+    origen: "Asgard",
+  },
+  {
+    nombreSuperheroe: "Hulk",
+    nombreReal: "Bruce Banner",
+    primeraAparicion: "The Incredible Hulk (2008)",
+    poder: "Fuerza descomunal, resistencia extrema y regeneración acelerada",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Black Widow",
+    nombreReal: "Natasha Romanoff",
+    primeraAparicion: "Iron Man 2 (2010)",
+    poder: "Habilidad en combate cuerpo a cuerpo, sigilo y espionaje",
+    origen: "Rusia",
+  },
+  {
+    nombreSuperheroe: "Spider-Man",
+    nombreReal: "Peter Parker",
+    primeraAparicion: "Captain America: Civil War (2016)",
+    poder: "Agilidad, fuerza mejorada, sentido arácnido y lanzar telarañas",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Doctor Strange",
+    nombreReal: "Stephen Strange",
+    primeraAparicion: "Doctor Strange (2016)",
+    poder: "Magia y manipulación de la realidad a través de hechizos",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Black Panther",
+    nombreReal: "T'Challa",
+    primeraAparicion: "Captain America: Civil War (2016)",
+    poder:
+      "Fuerza, agilidad y sentidos mejorados gracias a la hierba en forma de corazón",
+    origen: "Wakanda",
+  },
+  {
+    nombreSuperheroe: "Captain Marvel",
+    nombreReal: "Carol Danvers",
+    primeraAparicion: "Captain Marvel (2019)",
+    poder: "Superfuerza, vuelo y manipulación de energía cósmica",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Loki",
+    nombreReal: "Loki Laufeyson",
+    primeraAparicion: "Thor (2011)",
+    poder: "Dios del engaño, ilusiones, teletransportación y magia asgardiana",
+    origen: "Jotunheim / Asgard",
+  },
+  {
+    nombreSuperheroe: "Ant-Man",
+    nombreReal: "Scott Lang",
+    primeraAparicion: "Ant-Man (2015)",
+    poder:
+      "Cambio de tamaño gracias a las partículas Pym, comunicación con hormigas",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Wasp",
+    nombreReal: "Hope van Dyne",
+    primeraAparicion: "Ant-Man and the Wasp (2018)",
+    poder: "Cambio de tamaño, vuelo y blásters de energía",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Falcon",
+    nombreReal: "Sam Wilson",
+    primeraAparicion: "Captain America: The Winter Soldier (2014)",
+    poder: "Uso de alas mecánicas para volar, combate avanzado",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Winter Soldier",
+    nombreReal: "Bucky Barnes",
+    primeraAparicion: "Captain America: The First Avenger (2011)",
+    poder: "Fuerza y resistencia aumentadas, brazo biónico",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Scarlet Witch",
+    nombreReal: "Wanda Maximoff",
+    primeraAparicion: "Avengers: Age of Ultron (2015)",
+    poder: "Magia del caos, manipulación de la realidad, telequinesis",
+    origen: "Sokovia",
+  },
+  {
+    nombreSuperheroe: "Vision",
+    nombreReal: "Vision",
+    primeraAparicion: "Avengers: Age of Ultron (2015)",
+    poder: "Vuelo, intangibilidad, rayo de la Gema de la Mente",
+    origen: "Creado por Tony Stark, Bruce Banner y Ultron",
+  },
+  {
+    nombreSuperheroe: "Hawkeye",
+    nombreReal: "Clint Barton",
+    primeraAparicion: "Thor (2011)",
+    poder: "Experto arquero y combatiente",
+    origen: "Estados Unidos",
+  },
+  {
+    nombreSuperheroe: "Star-Lord",
+    nombreReal: "Peter Quill",
+    primeraAparicion: "Guardians of the Galaxy (2014)",
+    poder:
+      "Habilidades de combate y pilotaje, medio-celestial con resistencia aumentada",
+    origen: "Estados Unidos / Espacio",
+  },
+  {
+    nombreSuperheroe: "Gamora",
+    nombreReal: "Gamora",
+    primeraAparicion: "Guardians of the Galaxy (2014)",
+    poder: "Fuerza, agilidad y habilidades de combate sobrehumanas",
+    origen: "Zen-Whoberi",
+  },
+  {
+    nombreSuperheroe: "Drax",
+    nombreReal: "Drax el Destructor",
+    primeraAparicion: "Guardians of the Galaxy (2014)",
+    poder: "Fuerza sobrehumana y resistencia",
+    origen: "Kylos",
+  },
+  {
+    nombreSuperheroe: "Rocket Raccoon",
+    nombreReal: "Rocket",
+    primeraAparicion: "Guardians of the Galaxy (2014)",
+    poder: "Estratega, experto en armas y tecnología",
+    origen: "Contraxia (experimento genético)",
+  },
+  {
+    nombreSuperheroe: "Groot",
+    nombreReal: "Groot",
+    primeraAparicion: "Guardians of the Galaxy (2014)",
+    poder: "Fuerza sobrehumana, regeneración y crecimiento",
+    origen: "Planeta X",
+  },
+  {
+    nombreSuperheroe: "Mantis",
+    nombreReal: "Mantis",
+    primeraAparicion: "Guardians of the Galaxy Vol. 2 (2017)",
+    poder: "Empatía, manipulación emocional",
+    origen: "Criada por Ego el Planeta Viviente",
+  }
+];
+
+
+
